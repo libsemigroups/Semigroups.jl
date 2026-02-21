4ad610fdcf903b61e782ccbf06709f79212fcf90
James Swent <jake@osprey21.com>
2026-02-21 11:56:43 +0000

    ci: fix docs preview for external PRs

--- docs/make.jl
+++ docs/make.jl
@@ -48,8 +48,10 @@ makedocs(;
     warnonly = [:missing_docs],
 )
 
-deploydocs(
-    repo = "github.com/libsemigroups/Semigroups.jl.git",
-    devbranch = "main",
-    push_preview = true,
-)
+if get(ENV, "DOCS_DEPLOY", "false") == "true"
+    deploydocs(
+        repo = "github.com/libsemigroups/Semigroups.jl.git",
+        devbranch = "main",
+        push_preview = false,
+    )
+end
